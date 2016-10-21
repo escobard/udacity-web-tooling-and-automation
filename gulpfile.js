9ee48f06d9dd66102c7ebf66c285c6e89b785934
Daniel Escobar <escobard90@gmail.com>
2016-10-21 17:26:28 -0600

    Added sass function

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('default',function(){
 	console.log('Hello World');
 });
-//
+
+gulp.task('styles',function (){
+	gulp.src('sass/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('./css'));
+});
